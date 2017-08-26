d5a3fe8312a73543491c5fc6404377cd7ed03d95
anny1310 <anny1310@mail.ru>
2017-08-26 13:11:42 -0400

    initial check

---
+++ src/test/App.java
@@ -0,0 +1,10 @@
+package test;
+
+public class App {
+	
+	//String name = signUpPage.accountName.getAttribute("value");
+	//String lastName = signUpPage.accountLastName.getAttribute("value");
+	//String phoneNumber = signUpPage.phoneNumb.getAttribute("value");
+
+
+}
